872942ef8dc0ccbdb1f53214110910a2cb0bfdd5
Oksenoyt <a.oksenoyt@gmail.com>
2022-09-23 20:48:34 +0400

    Initial Commit

---
+++ ContactsListUI/ContactsListUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactsListUIApp.swift
+//  ContactsListUI
+//
+//  Created by Elenka on 23.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct ContactsListUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
